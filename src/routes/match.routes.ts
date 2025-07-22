3e000987b9c84ae1a6ac2d0840b81bbd7fb69564
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-22 16:50:03 -0400

    inclui middleware e padronização de nomeclatura

--- src/routes/match.routes.ts
+++ src/routes/match.routes.ts
@@ -1,20 +1,22 @@
 import { Router } from 'express';
 import { MatchController } from '../controllers/match.controller';
+import { validateMatch } from "../middlewares/match.validator.middleware";
+import { validateFinishMatch } from '../middlewares/finish.validator.middleware';
 
 const router = Router();
 
 // CRUD básico de partidas
-router.get('/', MatchController.getAll);
-router.post('/', MatchController.create);
+router.get('/', MatchController.getAllMatches);
+router.post('/', validateMatch, MatchController.createNewMatch);
 router.get('/open', MatchController.getOpenMatches);
 router.get('/history/:playerId', MatchController.getPlayerHistory);
 
 // Entrar/Sair da partida
-router.post('/:matchId/join/:playerId', MatchController.joinMatch);
-router.post('/:matchId/leave/:playerId', MatchController.leaveMatch);
+router.post('/:matchId/join/:playerId', MatchController.addPlayerToMatch);
+router.post('/:matchId/leave/:playerId', MatchController.removePlayerFromMatch);
 
 // Iniciar/Finalizar partida
-router.post('/:matchId/start', MatchController.startMatch);
-router.post('/:matchId/finish', MatchController.finishMatch);
+router.post('/:matchId/start', MatchController.startMatchById);
+router.post('/:matchId/finish', validateFinishMatch, MatchController.finishMatchById);
 
 export default router;
