274db7f42dbba4a78f48f04213a71eabad2cdb12
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-21 22:30:36 -0400

    atualiza e padroniza playerController

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,30 +1,80 @@
 import { Request, Response } from 'express';
+import { Prisma } from "@prisma/client";
+
 import { PlayerRepository } from '../repositories/player.repository';
 
 export const PlayerController = {
-  getAll: (req: Request, res: Response) => {
-    res.json(PlayerRepository.findAll());
-  },
-  create: (req: Request, res: Response) => {
-    const { name, nickname, email, matchId } = req.body;
-    if (!name || !nickname || !email || !/^\S+@\S+\.\S+$/.test(email)) {
-      return res.status(400).json({ error: 'Invalid data' });
+  getPlayersList: async (req: Request, res: Response) => {
+    try {
+      const players = await PlayerRepository.findAllPlayers();
+
+      if (players.length === 0) {
+        return res.status(404).json({ message: "Nenhum jogador encontrado" });
+      }
+
+      const sanitizedPlayers = players.map(({ id, ...rest }) => rest);
+
+      res.json(sanitizedPlayers);
+    } catch (error) {
+      res.status(500).json({ message: "Erro interno do servidor" });
     }
-    const player = PlayerRepository.create({ name, nickname, email, matchId });
-    res.status(201).json(player);
   },
-  getById: (req: Request, res: Response) => {
-    const player = PlayerRepository.findById(req.params.id);
-    if (!player) return res.status(404).json({ error: 'Player not found' });
-    res.json(player);
+
+  createPlayer: async (req: Request, res: Response) => {
+    const { name, nickname, email, matchId } = req.body;
+
+    try {
+      const player = await PlayerRepository.createPlayer({ name, nickname, email, matchId });
+
+      return res.status(201).json(player);
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+              const field = Array.isArray(error.meta?.target) ? error.meta.target[0] : undefined;
+
+              if (field === "email") {
+                return res.status(400).json({ error: "Este e-mail já está em uso." });
+
+              } else if (field === "nickname") {
+                return res.status(400).json({ error: "Este nickname já está em uso." });
+              }
+        }
+      }
+      return res.status(500).json({ error: "Erro interno no servidor." });
+    }
   },
-  update: (req: Request, res: Response) => {
-    const updated = PlayerRepository.update(req.params.id, req.body);
-    if (!updated) return res.status(404).json({ error: 'Player not found' });
-    res.json(updated);
+
+  getPlayerById: async (req: Request, res: Response) => {
+    try {
+      const player = await PlayerRepository.findPlayerById(req.params.id);
+
+      if (!player) return res.status(404).json({ error: 'Jogador não localizado!' });
+
+      res.status(200).json(player);
+    } catch (error) {
+      res.status(500).json({ message: "Erro interno do servidor" });
+    }
   },
-  remove: (req: Request, res: Response) => {
-    PlayerRepository.delete(req.params.id);
-    res.status(204).send();
+
+  updatePlayerById: async (req: Request, res: Response) => {
+    try {
+      const updated = await PlayerRepository.updatePlayerById(req.params.id, req.body);
+      
+      if (!updated) return res.status(404).json({ error: 'Jogador não localizado!' });
+
+      res.status(200).json(updated);
+    } catch (error) {
+      res.status(500).json({ message: "Erro interno do servidor" });
+    }
+  },
+
+  deletePlayerById: async (req: Request, res: Response) => {
+    try {
+      await PlayerRepository.deletePlayerById(req.params.id);
+
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ message: "Erro interno do servidor" });
+    }
   }
 };
