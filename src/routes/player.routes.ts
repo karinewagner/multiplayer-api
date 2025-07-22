bb6ee232aa0d83c6f773536fa9f3befa1038b250
Karine Débora Wagner <karinedwagner@gmail.com>
2025-07-21 22:32:29 -0400

    atualiza e padroniza playerRoutes

--- src/routes/player.routes.ts
+++ src/routes/player.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { PlayerController } from '../controllers/player.controller';
+import { validatePlayer } from '../middlewares/player.validator.middleware';
 
 const router = Router();
 
-router.get('/', PlayerController.getAll);
-router.post('/', PlayerController.create);
-router.get('/:id', PlayerController.getById);
-router.put('/:id', PlayerController.update);
-router.delete('/:id', PlayerController.remove);
+router.get('/', PlayerController.getPlayersList);
+router.post('/', validatePlayer, PlayerController.createPlayer);
+router.get('/:id', PlayerController.getPlayerById);
+router.put('/:id', PlayerController.updatePlayerById);
+router.delete('/:id', PlayerController.deletePlayerById);
 
 export default router;
